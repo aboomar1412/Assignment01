346057d648a28d1100bf7c232bc2ba2566f36649
Eias <aboomar5464@gmail.com>
2025-09-21 15:34:00 -0700

    STEP 2 – Literal expressions

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ let currentMonth;
 let summerMonth;
 let myLibraryFunction;
 
+//STEP 2 – Literal expressions
+console.log(42);              // numeric
+console.log("Hello World");   // string
+console.log(true);            // boolean
+console.log(null);            // null
